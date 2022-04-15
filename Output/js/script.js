948087283bb2a90b9b109e0ec4c003a3e1e24ce7
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-15 17:02:13 +0600

    connect js file with html

---
+++ Output/js/script.js
@@ -0,0 +1,13 @@
+//1
+        // window.alert("I'm learning JS");
+
+        //2
+        document.write("I'm learning JS");
+        //3
+        document.getElementById("root").innerHTML= "I lovee JavaScript";
+        document.getElementById("root2").innerHTML= "I'm learning it.";
+
+        // 4 
+
+        console.log("This text will go to console");
+        console.log("Second line");
