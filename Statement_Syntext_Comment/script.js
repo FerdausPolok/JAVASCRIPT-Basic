b6af0e6d7c38575a8b67d9dc658d8429dc3c4f52
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-15 17:22:31 +0600

    More

---
+++ Statement_Syntext_Comment/script.js
@@ -0,0 +1,13 @@
+document.write("YOOOOOOOOOOOO") ;
+document.write('YOOOOOO "DSDSDSD" OOOOOO');
+document.write(67.555);
+
+//comment 
+
+// sdsdsdsd
+
+
+/*sdafasdfsdaf
+adsfasfasdf
+asfasfasfsa
+asfasdfasf*/
