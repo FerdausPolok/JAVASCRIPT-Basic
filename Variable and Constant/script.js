946df7ca6719934a29b5c7e31397fa187b12189c
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-19 12:00:49 +0600

    Done

---
+++ Variable and Constant/script.js
@@ -0,0 +1,70 @@
+var a= 45;
+let b = "Year old";
+console.log(a);
+console.log(b);
+a=50;
+console.log(a);
+a= "My name is polok"
+console.log(a);
+
+var x=20;
+var y=x;
+
+console.log(x);
+console.log(y);
+
+x=30;
+
+console.log("new x:",x);
+console.log("new y:",y);
+
+//rule of naming variable
+
+//valid Variables
+var $a;
+var _a;
+var abc123;
+var MyName, myName;
+
+MyName = "Polok"
+myName = "Ferdaus"
+
+console.log(myName);
+console.log(MyName);
+
+//Not valid Variables -> Starting with number like 5polok
+
+
+var v = 20;
+console.log(v);
+var v;
+console.log(v);
+
+
+console.log("Contant and keyword part starts->")
+//constant and keywords
+
+//constant mane holo non changes value
+//declaration er sathe sathe value dite hobe
+
+const con1=2;
+
+console.log(con1);
+
+//keywords
+
+//var
+//const
+//they are js reserved word for js, we can't use them as any variable name or contact
+
+let a;
+let b=a;
+a=90;
+console.log(b)
+
+
+
+
+
+
+
