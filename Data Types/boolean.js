894f3d090c139f890ef1bf67613b8f7da79a2f10
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-23 16:06:24 +0600

    Data type- Boolean

---
+++ Data Types/boolean.js
@@ -0,0 +1,17 @@
+console.log("Starting boolean")
+
+x = 10<9
+console.log(x)
+
+y = true
+console.log(y)
+
+//boolean return - undefined, null, nan egulor jonne always boolean false return korbe
+
+
+a = "aaa"
+var ghg;
+
+console.log(Boolean(a))
+console.log(Boolean(ghg))
+
