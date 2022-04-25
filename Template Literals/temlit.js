c05dd98f5b0279cd009c380e178c7de3d25a1ea0
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-25 11:32:32 +0600

    Template Literals

---
+++ Template Literals/temlit.js
@@ -0,0 +1,40 @@
+// Template literal (ES6) is a feature of ES6 which helps to output multiple line of string and variable inside string
+
+//ES6 is a 2015 released version of JS
+
+//Till now
+
+console.log("First line")
+console.log("Secon line")
+
+console.log("First line \nSecond line")
+
+//tem lit using - using backtick ` instead of cotation "" 
+
+console.log(`First line
+Second line
+third line`)
+
+let tl= `First line
+Second line
+third line`
+
+console.log(tl)
+
+//normal variable inside Sting 
+
+age=36
+console.log("His age is", age)
+
+//using `
+let name= 'Rahim'
+console.log(`He is ${name} and
+His age is ${age}`)
+
+//We can also use expression
+
+p=10
+q=26
+
+console.log(`${p} + ${q} = ${p+q}`)
+
