238b1eb47e7db33a52d80de1f9bbdf9aa1c0ca9e
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-28 13:28:33 +0600

    String and Array Iteration

---
+++ Loops/String_Array_Iteration.js
@@ -0,0 +1,15 @@
+console.log("String and Array Iteration starts from here")
+
+Myname="Ferdaus Zaman Polok"
+
+console.log(Myname[0])
+
+for (i=0; i<Myname.length;i++){
+    console.log(Myname[i])
+}
+
+food=['rice', 'egg', 'dall']
+
+for (i=0; i<food.length;i++){
+    console.log(food[i])
+}
