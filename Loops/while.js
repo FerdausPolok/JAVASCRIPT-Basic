09a93ccee21009b2d8c504e66d22f61b35af898b
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-28 12:42:08 +0600

    While loop

---
+++ Loops/while.js
@@ -0,0 +1,29 @@
+console.log("While loops starts from here")
+
+x= 5
+a= 1
+while(a<=x){
+    console.log("Value of a is",a)
+    a++
+}
+
+console.log("Out of the loop")
+
+//while willl not work if
+
+while(0){
+    console.log("Check Print")
+}
+
+while(false){
+    console.log("Check Print")
+}
+
+//Do while loop- condition last a hobe and akta semicolon hobe
+
+//Do while loop is same as while but will run 1 time even if the condition is false
+do{
+   console.log("Executing do while loop")
+
+} while(false);
+
