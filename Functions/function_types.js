10707feffc7985a7ad9afa00722ea307cad67c0c
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-04-30 17:38:05 +0600

    Functions intro

---
+++ Functions/function_types.js
@@ -0,0 +1,23 @@
+//Two Types of function
+//Build-in and User-defined
+
+console.log("Build in funtions")
+
+//thses helps us to do spesific jobs
+//they are pre made by js developers 
+
+//like:
+
+//alert();
+console.log();
+
+console.log("User Defined Function")
+//Nijer ja lage banay nibo
+
+function SaySomething(text){
+    console.log("Finction is now activated")
+    console.log(text)
+}
+
+SaySomething("Hello World")
+
