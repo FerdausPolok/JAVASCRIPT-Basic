450d98f51ed4e5a0ad624ffb4d93f92e514bb889
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-21 12:11:50 +0600

    UPDATED DOM Selector (Single Element)

--- Document Object Model/DOM_Selector_SingleElement.js.js
+++ Document Object Model/DOM_Selector_SingleElement.js.js
@@ -1,26 +1,48 @@
-// document.getElementByID() -> Takes single element 
+console.log("DOM Selector Single Item Starts from here");
 
-console.log("DOM Selectore - Single item")
+// document.getElementByID() -> Takes single element by id
 
 let val2;
 
-
+//Geeting an Element
+val2 = document.getElementById('document-title');
+val2 = document.getElementById('document-title').id;
+val2 = document.getElementById('document-title').className;
+val2 = document.getElementById('document-title').classList;
+//Multiple id same hole 1st element anbe
 
 //Changing style
-// document.getElementById('document-title').style.background= '#333'
+document.getElementById('document-title').style.background= '#333';
+document.getElementById('document-title').style.color='#fff';
+document.getElementById('document-title').style.padding='10px'
+document.getElementById('document-title').style.display='none'
+document.getElementById('document-title').style.display='block';
 
-//document.querySelector()
+//Changing content
+document.getElementById('document-title').textContent= 'New Title chnaged by JS';
+document.getElementById('document-title').innerText='New innter text using js'
+document.getElementById('document-title').innerHTML= '<i> Again new Title by JSS </i>'
 
-val= document.querySelector('#document-title')
-console.log(val2)
+//We can store document.getElementById('document-title') in a variable
 
-val= document.querySelector('.title-class')
-console.log(val2)
+// querySelector()
+// can call with ID/ Class/ Tag
 
-val= document.querySelector('h3')
-console.log(val2)
+vall= document.querySelector('#document-title'); //# for ID
+vall= document.querySelector('.title-class'); //. for ClassName
+vall= document.querySelector('h3'); //Tag simple ja tai dile hobe
 
-//Getting an Element
-console.log("Hi")
-val2 = document.getElementById("document-title");
-console.log(val2)
+//list
+vall= document.querySelector('ol');
+vall= document.querySelector('li');
+//Paranting for geeting specific item 
+
+//access last child
+vall= document.querySelector('li:last-child');
+
+//access nth child
+vall= document.querySelector('li:nth-child(4)');
+vall.innerText="JS"
+
+console.log(vall);
+
