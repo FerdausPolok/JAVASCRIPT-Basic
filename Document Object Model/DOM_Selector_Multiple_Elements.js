b9b44aeb1bf9e40af547b50c3767e12c3fe4fade
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-22 08:10:16 +0600

    Traversing

--- Document Object Model/DOM_Selector_Multiple_Elements.js
+++ Document Object Model/DOM_Selector_Multiple_Elements.js
@@ -7,7 +7,7 @@ let list = document.getElementsByClassName('sample-class')
 console.log(list);
 console.log(list[0]);
 list[0].style.color = 'white';
-list[0].style.background= 'red'
+list[0].style.background= 'black'
 list[0].style.padding = '10px';
 list[0].textContent = 'Hello World!';
 
@@ -53,7 +53,7 @@ liOdd.forEach(item =>{
     item.style.background= 'grey'
 })
 liEven.forEach(item =>{
-    item.style.background= 'red'
+    item.style.background= 'black'
     item.style.color= 'white'
 })
 
--- Document Object Model/Traversing.js
+++ Document Object Model/Traversing.js
@@ -1,8 +1,58 @@
-console.log("Traversing TEH DOM")
+console.log("Traversing Starts from here")
 
-let val3
+let val3;
+list = document.querySelector('ul'); 
+listItem = document.querySelector('ul li:first-child');
 
-let list = document.querySelector('ul');
-val3= list;
+
+val3= list
+val3= listItem 
+
+//Get the Child Node
+val3 = list
+val3 = list.childNodes;
+val3 = list.childNodes[0]; //Text for Keyboard enter
+val3 = list.childNodes[1];
+val3 = list.childNodes[2];
+val3 = list.childNodes[1].nodeName;
+val3 = list.childNodes[1].nodeType;
+
+//NodeTyoe:
+// 1 Element
+// 2 Attribute
+// 3 Text Node
+// 8 Comment
+// Document Itself
+// 10 Doctype
+
+//without text node
+val3 = list.children;
+val3 = list.children[2];
+
+list.children[2] = "Hello"
+
+list.children[1].children[0].innerHTML= `<a href="https://neetcode.io/">NeetCode</a>`
+
+val3= list.children[1].children[0].href;
+
+val3 = list.firstChild;
+val3 = list.firstElementChild;
+val3 = list.lastChild;
+val3 = list.lastElementChild;
+
+val3= list.childElementCount;
+
+val3= listItem
+val3= listItem.children
+val3= listItem.parentElement;
+val3= listItem.parentElement.parentElement;
+
+val3= listItem.nextSibling;
+val3= listItem.nextElementSibling;
+val3= listItem.nextElementSibling.nextElementSibling;
+
+val3 = document.querySelector('ul li:last-child');
+val3= val3.previousSibling;
+val3= val3.parentElementSibling;
 
 console.log(val3)
