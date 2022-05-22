774f832a4196477ffe0b4c98fdfee251b0a4d696
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-22 11:07:46 +0600

    More on Events

--- Document Object Model/events.js
+++ Document Object Model/events.js
@@ -10,8 +10,28 @@ function message(){
 //do the same job in js to get a clear html
 
 let btn = document.getElementById('click');
-btn.addEventListener('mouseover', onClickFun) //event name,  function name //on bad diye likhbo
+btn.addEventListener('click', onClickFun) //event name,  function name //on bad diye likhbo
 
-function onClickFun(){
+function onClickFun(e){
     console.log("Mouse over button from js");
+    console.log(e)
+
+    val = e.timeStamp;
+
+    console.log(val);
+
+}
+
+//form
+
+document.querySelector("#name").addEventListener('focus', test);
+document.querySelector("#name").addEventListener('keyup', test2);
+
+function test(e){
+    console.log("focued");
+    this.style.background = 'pink'
+}
+
+function test2(e){
+    document.getElementById("demo").innerText= this.value;
 }
